c74d6fb2edf7228da8f023d6c8ae85b2c4c6d689
NehorayLahmi <xr329117@gmail.com>
2026-05-31 16:40:39 +0300

    ביטול סופי של מפות בווב

---
+++ client/mocks/react-native-maps.js
@@ -0,0 +1,10 @@
+const { View } = require('react-native');
+
+const MapView = View;
+const Marker = View;
+const Circle = View;
+
+module.exports = MapView;
+module.exports.default = MapView;
+module.exports.Marker = Marker;
+module.exports.Circle = Circle;
